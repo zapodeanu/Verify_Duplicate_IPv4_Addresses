6a079d80c12667c7ab06c3cbac63128b76c016d6
gzapodea <gzapodea@cisco.com>
2019-06-03 14:27:13 -0700

    updated the Readme file

---
+++ config.py
@@ -0,0 +1,15 @@
+
+
+# developed by Gabi Zapodeanu, TME, Enterprise Networks, Cisco Systems
+
+
+# This file contains the:
+# DNAC username and password, server info
+
+
+# Update this section with the DNA Center server info and user information
+DNAC_URL = 'https://10.1.1.1'
+DNAC_USER = 'admin'
+DNAC_PASS = 'Cisco123!'
+
+
